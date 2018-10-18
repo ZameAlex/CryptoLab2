3372bb93b502764b50f12e017158a055abd67f29
Alex <Alex@DESKTOP-3MOEDP1>
2018-10-18 05:58:07 +0300

    Init commit

---
+++ DES/ICommonProcess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DES
+{
+    interface ICommonProcess
+    {
+        string EncryptionStart(string text, string key, bool IsTextBinary);
+        string DecryptionStart(string text, string key, bool IsTextBinary);
+    }
+}
---
+++ TripleDES/TripleDES.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DES;
+
+namespace TripleDES
+{
+    public class TripleDES
+    {
+        #region Encryption
+        public string Encrypt_DES_EEE3(string text)
+        {
+            return text;
+        }
+
+        public string Encrypt_DES_EDE3(string text)
+        {
+            return text;
+        }
+
+        public string Encrypt_DES_EDE2(string text)
+        {
+            return text;
+        }
+        #endregion Encryption
+
+        #region Decryption
+        public string Decrypt_DES_EEE3(string text)
+        {
+            return text;
+        }
+
+        public string Decrypt_DES_EDE3(string text)
+        {
+            return text;
+        }
+
+        public string Decrypt_DES_EDE2(string text)
+        {
+            return text;
+        }
+        #endregion Decryption
+
+
+    }
+}
